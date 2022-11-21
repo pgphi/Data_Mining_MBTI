de1aa78b4512e1bede1ca5a0033794bc3cd50b77
Philipp Ganster <85439878+pgphi@users.noreply.github.com>
2022-11-21 17:22:45 +0100

    preprocessing and exploration
    - updated preprocessing function (now preparation)
    - created classification.py for classifiers
    - created datasets for binary and multiclassification

--- preparation.py
+++ preparation.py
@@ -9,21 +9,25 @@ from nltk.corpus import stopwords
 import nltk
 from nltk.stem import WordNetLemmatizer
 
-# import raw dataset
-df = pd.read_csv("data/mbti_1.csv")
-print(df.head())
+
+def preprocessing(posts):
+
+    """
+    :param posts: one row of raw text column of dataset. This function will be applied to pandas DataFrame and
+    iterated through everyrow with lambda function.
+    :return: cleaned text <string> column of dataset for every raw text row.
+    """
+
+    # print("raw post: \n" + posts)
+
+    # preprocess raw data
+
+    # Remove non-alpha characters and preservers whitespaces (me420 i'm done that's it!!! --> me im done thats it)
+    posts = posts.replace("|||", " ")
+    # print("remove non-alpha chars and whitespaces: \n" + posts)
 
 
-# preprocess raw data
-
-# Remove non-alpha characters and preservers whitespaces (me420 i'm done that's it!!! --> me im done thats it)
-def split_posts(posts: str) -> str:
-    return posts.replace("|||", " ")
-
-
-# Replaces URLs with Second-Level-Domain (https://www.youtube.com/watch?v=dQw4w9WgXcQ --> youtube)
-def replace_url_with_domain(posts: str) -> str:
-
+    # Replaces URLs with Second-Level-Domain (https://www.youtube.com/watch?v=dQw4w9WgXcQ --> youtube)
     url_list = re.findall(r'(https?://[^\s]+)', posts)
 
     if len(url_list) > 0:
@@ -31,23 +35,30 @@ def replace_url_with_domain(posts: str) -> str:
             domain = tldextract.extract(url).domain
             posts = posts.replace(url, domain)
 
-    return posts
+    # print("replace urls with domain name: \n" + posts)
 
 
-# Corrects expressive lengthening / word lengthening (hellooo --> hello)
-def correct_expressive_lengthening(posts: str) -> str:
-    re.sub(r'(.)\1{3,}', r'\1', posts)
-    return posts
+    # handle emojis regex match 2 colons : around a string and replace with just the string
+    re.sub(r':(.*?):', r'\1' + "emoji", posts)
+    # print("turn emojis into string: \n" + posts)
 
 
-# Tokenization: ["Hello my name is"] --> ["Hello", "my", "name", "is"]
-def tokenize_posts(posts: str) -> list[str]:
+    # Removes non alphabet chars and \s for matching whitespace
+    regex = re.compile('[^a-zA-Z\s]')
+    posts = regex.sub('', posts)
+    # print("remove more non-alpha chars: \n" + posts)
+
+
+    # Corrects expressive lengthening / word lengthening (hellooo --> hello)
+    posts = re.sub(r'(.)\1{3,}', r'\1', posts)
+    # print("correct expressive length: \n" + posts)
+
+    # Tokenization: ["Hello my name is"] --> ["Hello", "my", "name", "is"]
     tokens = nltk.word_tokenize(posts.lower())
-    return tokens
+    # print("create tokens: \n" + str(tokens))
 
 
-# remove stopwords i.e. "and"
-def remove_stopwords(tokens):
+    # remove stopwords i.e. "and"
     stopwords_ = set(stopwords.words("english"))
 
     # https://escholarship.org/content/qt6n5652cx/qt6n5652cx.pdf
@@ -59,38 +70,56 @@ def remove_stopwords(tokens):
     for token in tokens:
         if token not in custom_stopwords:
             clean_tokens.append(token)
+    # print("stopwords removed tokens: \n" + str(clean_tokens))
 
-    return clean_tokens
-
-
-# Keep context of word i.e. changing --> change (instead of changing --> chang, when using stemming)
-def lemmatize_posts(tokens):
+    # Keep context of word i.e. changing --> change (instead of changing --> chang, when using stemming)
     lemmatizer = WordNetLemmatizer()
     lemmas = []
     for w in tokens:
         # print("Lemma for {} is {}".format(w, lemmatizer.lemmatize(w)))
         lemmas.append(lemmatizer.lemmatize(w))
-    return lemmas
+    # print("lemmatized text corpus: \n" + str(lemmas))
+
+    # concat lemmas back to text
+    clean_text = " ".join(lemmas)
+
+    return clean_text
 
 
-# Data exploration
-def explore(corpus):
-    frequency_distribution = FreqDist(corpus)
+# Data Exploration
+def exploration(df, n):
+
+    """
+    :param df: rows of preprocessed text column from dataset
+    :param n: top N-frequent words
+    :return: top N-words frequency graph and wordcloud of preprocessed text
+    """
+
+    for i in df:
+        corpus = "".join(i)
+
+    tokens = nltk.word_tokenize(corpus.lower())
+    # print(tokens)
+
+    frequency_distribution = FreqDist(tokens)
+    print(frequency_distribution)
     plt.ion()
-    frequency_distribution.plot(30, title="Most 30-Words for " + type + " Types", cumulative=False)
-    plt.savefig("img/" + type + "_frequency.png")
+    frequency_distribution.plot(n, title="Most " + str(n) + "-Words in corpus", cumulative=False)
+    plt.savefig("img/" + "word_frequency.png")
     plt.ioff()
     plt.show()
     plt.ion()
-    word_cloud = WordCloud(collocations=False, background_color='white').generate()
+
+    word_cloud = WordCloud(collocations=False, background_color='white').generate(corpus)
     plt.imshow(word_cloud, interpolation='bilinear')
     plt.axis("off")
-    plt.savefig("img/" + type + "_wordcloud.png")
+    plt.savefig("img/" + "corpus_wordcloud.png")
     plt.ioff()
     plt.show()
 
 
-def group_prepro_data(df):
+# MBTI Personality Types in different CSV files
+def group_data(df):
     """
     :param df: (preprocessed) data
     :return: create csv file for every MBTI type
@@ -103,107 +132,105 @@ def group_prepro_data(df):
         if perso_type == "ENFJ":
             df_ENFJ = pd.DataFrame(data)
             df_ENFJ.to_csv("data/personality_types/df_ENFJ.csv")
-            print(perso_type)
-            print(df_ENFJ)
-            print(data["posts"])
+            # print(perso_type)
+            # print(df_ENFJ)
+            # print(data["posts"])
 
         elif perso_type == "ENFP":
             df_ENFP = pd.DataFrame(data)
             df_ENFP.to_csv("data/personality_types/df_ENFP.csv")
-            print(perso_type)
-            print(df_ENFP)
+            # print(perso_type)
+            # print(df_ENFP)
 
         elif perso_type == "ENTJ":
             df_ENTJ = pd.DataFrame(data)
 
             df_ENTJ.to_csv("data/personality_types/df_ENTJ.csv")
-            print(perso_type)
-            print(df_ENTJ)
+            # print(perso_type)
+            # print(df_ENTJ)
 
         elif perso_type == "ENTP":
             df_ENTP = pd.DataFrame(data)
             df_ENTP.to_csv("data/personality_types/df_ENTP.csv")
-            print(perso_type)
-            print(df_ENTP)
+            # print(perso_type)
+            # print(df_ENTP)
 
         elif perso_type == "ESFJ":
             df_ESFJ = pd.DataFrame(data)
             df_ESFJ.to_csv("data/personality_types/df_ESFJ.csv")
-            print(perso_type)
-            print(df_ESFJ)
+            # print(perso_type)
+            # print(df_ESFJ)
 
         elif perso_type == "ESFP":
             df_ESFP = pd.DataFrame(data)
             df_ESFP.to_csv("data/personality_types/df_ESFP.csv")
-            print(perso_type)
-            print(df_ESFP)
+            # print(perso_type)
+            # print(df_ESFP)
 
         elif perso_type == "ESTP":
             df_ESTP = pd.DataFrame(data)
             df_ESTP.to_csv("data/personality_types/df_ESTP.csv")
-            print(perso_type)
-            print(df_ESTP)
+            # print(perso_type)
+            # print(df_ESTP)
 
         elif perso_type == "INFJ":
             df_INFJ = pd.DataFrame(data)
             df_INFJ.to_csv("data/personality_types/df_INFJ.csv")
-            print(perso_type)
-            print(df_INFJ)
+            # print(perso_type)
+            # print(df_INFJ)
 
         elif perso_type == "INFP":
             df_INFP = pd.DataFrame(data)
             df_INFP.to_csv("data/personality_types/df_INFP.csv")
-            print(perso_type)
-            print(df_INFP)
+            # print(perso_type)
+            # print(df_INFP)
 
         elif perso_type == "INTJ":
             df_INTJ = pd.DataFrame(data)
             df_INTJ.to_csv("data/personality_types/df_INTJ.csv")
-            print(perso_type)
-            print(df_INTJ)
+            # print(perso_type)
+            # print(df_INTJ)
 
         elif perso_type == "INTP":
             df_INTP = pd.DataFrame(data)
             df_INTP.to_csv("data/personality_types/df_INTP.csv")
-            print(perso_type)
-            print(df_INTP)
+            # print(perso_type)
+            # print(df_INTP)
 
         elif perso_type == "ISFJ":
             df_ISFJ = pd.DataFrame(data)
             df_ISFJ.to_csv("data/personality_types/df_ISFJ.csv")
-            print(perso_type)
-            print(df_ISFJ)
+            # print(perso_type)
+            # print(df_ISFJ)
 
         elif perso_type == "ISFP":
             df_ISFP = pd.DataFrame(data)
             df_ISFP.to_csv("data/personality_types/df_ISFP.csv")
-            print(perso_type)
-            print(df_ISFP)
+            # print(perso_type)
+            # print(df_ISFP)
 
         elif perso_type == "ISTJ":
             df_ISTJ = pd.DataFrame(data)
             df_ISTJ.to_csv("data/personality_types/df_ISTJ.csv")
-            print(perso_type)
-            print(df_ISTJ)
+            # print(perso_type)
+            # print(df_ISTJ)
 
         elif perso_type == "ISTP":
             df_ISTP = pd.DataFrame(data)
             df_ISTP.to_csv("data/personality_types/df_ISTP.csv")
-            print(perso_type)
-            print(df_ISTP)
+            # print(perso_type)
+            # print(df_ISTP)
 
 
-# create dataset with binary labels
-def binary_labeling():
+# Create dataset with binary labels
+def binary_labeling(list):
+
     """
-    :param:
-    :return:
+    :param: List of csv files (personality types)
+    :return: one big csv file with summarized extroverted and introverted types
     Extroverted - 1
     Introverted - 0
     """
-
-    # List of all datasets
-    list = os.listdir("data/personality_types")
 
     # create csv datasets
     df_introverted_binary = pd.DataFrame()
@@ -288,29 +315,40 @@ def binary_labeling():
 
         # create csv files
         df_binary_all = df_introverted_binary.append(df_extroverted_binary)
-        df_binary_all.to_csv("data/df_binary_all.csv")
-        df_extroverted_binary.to_csv("data/df_extroverted_binary.csv")
-        df_introverted_binary.to_csv("data/df_introverted_binary.csv")
+        df_binary_all.to_csv("data/df_binary_preprocessed.csv")
 
 
-# call Functions
 
-# group_prepro_data(df)
-# binary_labeling()
-df["preprocessed_posts"] = df["posts"].apply(split_posts)
-print(df.head())
-df["preprocessed_posts"] = df["preprocessed_posts"].apply(replace_url_with_domain)
-print(df.head())
-df["preprocessed_posts"] = df["preprocessed_posts"].apply(correct_expressive_lengthening)
-print(df.head())
-df["preprocessed_posts"] = df["preprocessed_posts"].apply(tokenize_posts)
-print(df.head())
-df["preprocessed_posts"] = df["preprocessed_posts"].apply(remove_stopwords)
-print(df.head())
-df["preprocessed_posts"] = df["preprocessed_posts"].apply(lemmatize_posts)
-print(df.head())
-corpus = df["preprocessed_posts"].apply(lemmatize_posts) # create bag-of-words (corpus/vocabulary)
-print(corpus)
-# create csv dataset file and add the preprocessed column of text (posts) data
-df_preprocessed = df.to_csv("data/df_preprocessed.csv")
-print(df_preprocessed.head())
+# ----------------------------------------------- Call Functions ----------------------------------------------------
+
+# Variable for adjusting how many rows we work with (for testing purposes only! For production use length of dataset)
+N = 1000 # len of dataset 8675
+
+# import raw dataset
+df = pd.read_csv("data/mbti_1.csv")
+print("Raw Dataset")
+print(df.head() + "\n")
+
+
+# create new csv file of dataset with added preprocessed text data
+df["preprocessed_text"] = df["posts"][0:N].apply(lambda x: preprocessing(x))
+df.to_csv("data/df_multi_preprocessed.csv")
+print("Preprocessed Multiclassification Dataset")
+print(df.head() + "\n")
+
+
+# Data Exploration
+dataset = df["preprocessed_text"][0:N]
+exploration(dataset, 50)
+
+
+# Create different MBTI personality groups and output them as csv files
+group_data(df)
+
+
+# Label MBTI groups as extroverted (1) and introverted (0) in one big csv file
+list = os.listdir("data/personality_types")
+binary_labeling(list)
+df_binary = pd.read_csv("data/df_binary_preprocessed.csv")
+print("Preprocessed Binary Dataset")
+print(df_binary.head())
